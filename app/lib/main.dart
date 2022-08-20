9567e6494af01e8b2dfff0d02c76fe5fa0c9b60c
Martin Jainta <martin.jainta@gmail.com>
2022-08-20 15:19:12 +0200

    Add google_fonts so we dont have to download on each start

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'src/shared/app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(const App());
 }
